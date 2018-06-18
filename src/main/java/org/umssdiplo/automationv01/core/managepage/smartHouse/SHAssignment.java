8ee5fa3720a1d30500870f8d25ba761df69df74c
Tany <tany7a@gmail.com>
2018-06-17 20:00:17 -0400

    QA

---
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/smartHouse/SHAssignment.java
@@ -0,0 +1,30 @@
+package org.umssdiplo.automationv01.core.managepage.smartHouse;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.umssdiplo.automationv01.core.managepage.BasePage;
+
+public class SHAssignment extends BasePage {
+    @FindBy(name = "assignment_button")
+    private WebElement assignmentTab;
+    @FindBy(name = "new_assignment")
+    private WebElement addAssignmentButton;
+    @FindBy(name = "save_assignment_button")
+    private WebElement acceptNewAssignment;
+    @FindBy(name = "employeeKey")
+    private WebElement employeeKeyElement;
+    @FindBy(name = "equipmentKey")
+    private WebElement equipmentKeyElement;
+    @FindBy(name = "observations")
+    private WebElement observationsElement;
+
+    @FindBy(xpath = "//*[@id=\"mat-option-4\"]")
+    private WebElement employeeOption;
+    @FindBy(xpath = "/html/body/app-root/app-assignmentform/form/mat-card/mat-list/mat-list-item[1]/div/mat-input-container/div/div[1]/div")
+    private WebElement employeeSelect;
+
+    @FindBy(xpath = "//*[@id=\"mat-option-3\"]")
+    private WebElement equipmentOption;
+    @FindBy(xpath = "/html/body/app-root/app-assignmentform/form/mat-card/mat-list/mat-list-item[4]/div/mat-input-container/div/div[1]/div")
+    private WebElement equipmentSelect;
+}
--- src/test/resources/smartHouseFeatures/Mod1Case1/Assignment.feature
+++ src/test/resources/smartHouseFeatures/Mod1Case1/Assignment.feature
@@ -1,7 +1,6 @@
 Feature: Asignacion de equipos
   Background: El usuario ha sido logueado en el sistema
-              Existe al menos un empleado registrado en el sistema
-              Existe al menos un equipo registrado en el sistema
+
   Scenario: Asignar un equipo a un empleado
     Given: Cuando el usuario selecciona la opcion 'Asignación de Equipos' y selecciona el boton 'Nueva Asignación'
     When: El usuario selecciona el empleado <"employee"> el equipo a ser asignado <"equipment"> y observaciones <"observaciones">
