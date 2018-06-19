976b7b204d9dfeffc7314e138f5493b381b0f3d9
garyveraterrazas <garyver@hotmail.com>
2018-06-19 15:53:08 -0400

    refactorization

--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionMod3Case1.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionMod3Case1.java
@@ -3,15 +3,16 @@ package org.umssdiplo.automationv01.stepdefinitionproject;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import org.umssdiplo.automationv01.core.managepage.Login.Login;
+import org.umssdiplo.automationv01.core.managepage.smartHouse.Header;
 import org.umssdiplo.automationv01.core.managepage.smartHouse.SHLogin;
 import org.umssdiplo.automationv01.core.utils.LoadPage;
 
 public class StepsDefinitionMod3Case1 {
     private SHLogin login;
-
+    private Header menu;
     @Given("^'SMARTHOUSE' page is loaded$")
     public void phpTravelPageIsLoaded() throws Throwable {
-        login = LoadPage.smartHousePage();
+        menu = LoadPage.smartHousePage();
     }
     @And("^set admin credentials on 'Login' page$")
     public void setMyCredentialsOnLoginPage() throws Throwable {
