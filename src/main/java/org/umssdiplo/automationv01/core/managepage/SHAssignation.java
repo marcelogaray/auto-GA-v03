d5a60ff8f2515c20b9b8a09661be16eb701274d8
Tany <tany7a@gmail.com>
2018-06-22 00:31:53 -0400

    QA

--- src/main/java/org/umssdiplo/automationv01/core/managepage/SHAssignation.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/SHAssignation.java
@@ -1,5 +1,59 @@
 package org.umssdiplo.automationv01.core.managepage;
 
-public class SHAssignation extends BasePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.umssdiplo.automationv01.core.utils.CommonEvents;
+import org.umssdiplo.automationv01.core.utils.PropertyAccessor;
 
+public class SHAssignation extends BasePage {
+    @FindBy(name = "assignment_button")
+    private WebElement assignmentTab;
+
+    @FindBy(name = "new_assignment")
+    private WebElement addAssignmentButton;
+
+    @FindBy(name = "save")
+    private WebElement acceptNewAssignment;
+
+    @FindBy(className = "swal2-confirm swal2-styled")
+    private WebElement Ok;
+
+    @FindBy(name = "employee")
+    private WebElement employeeKeyElement;
+    @FindBy(name = "equipment")
+    private WebElement equipmentKeyElement;
+    @FindBy(name = "observations")
+    private WebElement observationsElement;
+
+    @FindBy(id = "mat-dialog-0")
+    private WebElement employeeOption;
+    @FindBy(id= "mat-option-13")
+    private WebElement employeeSelect;
+
+    @FindBy(id = "mat-dialog-0")
+    private WebElement equipmentOption;
+    @FindBy(id = "mat-option-0")
+    private WebElement equipmentSelect;
+
+
+
+    public void NuevaAsignacion(){
+        CommonEvents.clickButton(addAssignmentButton);
+    }
+    public void setData() {
+        String observations = PropertyAccessor.getInstance().getSHDataProperty("observations");
+        CommonEvents.jsClickElement(employeeKeyElement);
+        CommonEvents.jsClickElement(employeeSelect);
+
+        CommonEvents.jsClickElement(equipmentKeyElement);
+        CommonEvents.jsClickElement(equipmentSelect);
+        CommonEvents.setInputField(observationsElement, observations);
+    }
+    public void registerNewAssignment(){
+        CommonEvents.jsClickElement(acceptNewAssignment);
+    }
+    public void registeredAssignment(){
+        CommonEvents.jsClickElement(Ok);
+    }
 }
+
--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/AssignmentSteps.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/AssignmentSteps.java
@@ -32,17 +32,18 @@ public class AssignmentSteps {
 
     @When("^user selects an employee, a equipment and enters observations of the assignment made$")
     public void user_selects_an_employee_a_equipment_and_enters_observations_of_the_assignment_made() throws Throwable {
+        assignment.setData();
 
     }
 
     @When("^click over 'Crear' button on 'Asignar equipo' modal$")
     public void click_over_Crear_button_on_Asignar_equipo_modal() throws Throwable {
-
+        assignment.registerNewAssignment();
     }
 
     @Then("^'La asignacion fue registrada correctamente' information message should be displayed$")
     public void la_asignacion_fue_registrada_correctamente_information_message_should_be_displayed() throws Throwable {
-
+        assignment.registeredAssignment();
     }
 
 }
