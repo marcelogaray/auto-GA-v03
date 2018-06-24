74f48a956ad4f1b3443465ea0b80677b8ef43afd
natalynanda <natalynanda@gmail.com>
2018-06-24 11:24:22 -0400

    revert chrome not verision settings

--- src/main/java/org/umssdiplo/automationv01/core/customwebdriver/Chrome.java
+++ src/main/java/org/umssdiplo/automationv01/core/customwebdriver/Chrome.java
@@ -7,11 +7,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class Chrome implements CustomWebDriver {
     @Override
     public WebDriver generateWebDriver() {
-        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-             ChromeOptions options = new ChromeOptions();
-                options.setExperimentalOption("useAutomationExtension", false);
-                options.addArguments("--no-sandbox");
-                options.addArguments("--disable-dev-shm-usage");
-                return new ChromeDriver(options);
+        return new ChromeDriver();
     }
 }
