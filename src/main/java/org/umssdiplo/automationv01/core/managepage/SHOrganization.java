877349d097963e811bc4dacd808360c410b19092
garyveraterrazas <garyver@hotmail.com>
2018-06-23 22:29:18 -0400

    Init commit for ORG_Case5

--- src/main/java/org/umssdiplo/automationv01/core/managepage/SHOrganization.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/SHOrganization.java
@@ -1,4 +1,44 @@
 package org.umssdiplo.automationv01.core.managepage;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.umssdiplo.automationv01.core.utils.CommonEvents;
+import org.umssdiplo.automationv01.core.utils.PropertyAccessor;
+
 public class SHOrganization extends BasePage {
+    @FindBy(name = "org_button")
+    private WebElement organizationTab;
+
+    @FindBy(name = "org_1")
+    private WebElement organizationDetailButton;
+
+    @FindBy(name = "newOrganization_button")
+    private WebElement addOrganizationButton;
+
+    @FindBy(name = "del_org_2")
+    private WebElement removeOrganizationButton;
+
+    @FindBy(id = "swal2-content")
+    private WebElement messageAlert;
+
+    public void openOrganizationDetailView() {
+        CommonEvents.clickButton(organizationDetailButton);
+    }
+
+    public void openNewOrganizationForm() {
+        CommonEvents.clickButton(addOrganizationButton);
+    }
+
+    public void removeOrganization() {
+        CommonEvents.clickButton(removeOrganizationButton);
+    }
+
+    public String getAreaWithEmployeeMessage() {
+        String message = String.format(PropertyAccessor.getInstance().getAreaWithEMployeeMessage(), PropertyAccessor.getInstance().getAreaIdForRemove());
+        return message;
+    }
+
+    public String getAlertMessage() {
+        return CommonEvents.getTextContent(messageAlert);
+    }
 }
--- src/main/java/org/umssdiplo/automationv01/core/utils/PropertyAccessor.java
+++ src/main/java/org/umssdiplo/automationv01/core/utils/PropertyAccessor.java
@@ -10,6 +10,8 @@ public class PropertyAccessor {
     private static final String BASE_URL = "baseurl";
     private static final String USER = "smartHouseUsername";
     private static final String PASSWORD = "smartHousePassword";
+    private static final String AREA_ID_FOR_REMOVE = "areaIdToRemove";
+    private static final String MESSAGE_AREA_WITH_EMPLOYEE = "messageErrorAreaWithEmployee";
     private static PropertyAccessor PropertyAccessor;
     private Properties properties;
 
@@ -61,5 +63,13 @@ public class PropertyAccessor {
 
     public String getPassword() {
         return getDataProperty(PASSWORD);
+    }
+
+    public String getAreaIdForRemove() {
+        return getDataProperty(AREA_ID_FOR_REMOVE);
+    }
+
+    public String getAreaWithEMployeeMessage() {
+        return getDataProperty(MESSAGE_AREA_WITH_EMPLOYEE);
     }
 }
