d2d1d3383ea35db7afe293908f4ad5fbfc939964
Tany <tany7a@gmail.com>
2018-06-25 20:08:29 -0400

    AssiCase8

--- src/main/java/org/umssdiplo/automationv01/core/managepage/SHAssignModalDelete.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/SHAssignModalDelete.java
@@ -19,18 +19,22 @@ public class SHAssignModalDelete extends BasePage {
 
 
     public boolean isEditDialogPresent() {
+
         return editModal.isDisplayed();
     }
 
     public void deleteOnEditForm() {
+
         CommonEvents.clickButton(confirmDelete);
     }
 
     public boolean isConfirmModalPresent() {
+
         return confirmDelete.isDisplayed();
     }
 
     public void cancelModal(){
+
         CommonEvents.clickButton(cancelDelete);
     }
 }
