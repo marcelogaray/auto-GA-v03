71ed9e75b41462be6d2147024b2fc51af5cfd509
Yussen <yussen_sg@hotmail.com>
2018-06-30 15:53:44 -0400

    logout

--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/CommonSteps.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/CommonSteps.java
@@ -2,18 +2,15 @@ package org.umssdiplo.automationv01.stepdefinitionproject;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.testng.Assert;
-import org.umssdiplo.automationv01.core.managepage.*;
+import org.umssdiplo.automationv01.core.managepage.HeaderWithLogin;
+import org.umssdiplo.automationv01.core.managepage.HeaderWithoutLogin;
+import org.umssdiplo.automationv01.core.managepage.SHLogin;
 import org.umssdiplo.automationv01.core.utils.LoadPage;
 
 public class CommonSteps {
     private SHLogin login;
     private HeaderWithLogin headerWithLogin;
     private HeaderWithoutLogin headerWithoutLogin;
-    private SHAccident shAccident;
-    private SHAccidentFormModal shAccidentFomModal;
 
     @Given("^I loging to 'SMARTHOUSE' page")
     public void smarthouse_s_page_is_loaded() throws Throwable {
@@ -26,39 +23,9 @@ public class CommonSteps {
         headerWithLogin = login.fillCredentials();
     }
 
-    @And("^click on 'Accidentes' button on navigation NavigationBar$")
-    public void clik_On_Accidentes_On_Navigarion_Bar() throws Throwable {
-        shAccident = headerWithLogin.clickAccidentTab();
-    }
-
-    @When("^click on 'Registrar Accidente' button on Accident Page$")
-    public void click_On_Reg_Accident_Btn_On_Acc_Page() throws Throwable {
-        shAccidentFomModal = shAccident.ClickRegistarAccBtn();
-    }
-
-    @Then("^verify that 'Registro Accidente'Form Modal is Displayed$")
-    public void check_Accident_Modal_Form_Displayed() throws Throwable {
-        Assert.assertTrue(shAccidentFomModal.isModalDialogPresent(), "El Formulario no se Cargo Correctamente");
-    }
-
-    @And("^I close de 'Registro Accidente'Form Modal$")
-    public void close_Accident_Modal_Form() throws Throwable {
-        shAccidentFomModal.closeModalDialog();
-    }
-
     @And("^I logOut Form 'SMARTHOUSE' Page$")
     public void logOut_From_SMATHOUSE_Page() throws Throwable {
         headerWithoutLogin.openLoginPage();
-        login.clicklogOutButton();
-    }
-
-    @When("^required data is filled on 'Registro Accidente' Modal Form$")
-    public void fill_Modal_Form_With_Valid_Req_Data() throws Throwable {
-        shAccidentFomModal.fillModalFomFullData();
-    }
-
-    @Then("^'Crear' button from 'Registro Accidente' Modal Form  should be enable$")
-    public void verify_Crear_Btn_Modal_Fom_isEnable() throws Throwable {
-        Assert.assertFalse(shAccidentFomModal.isCrearBtnEnable(), "Error, El boton crear no esta habilitado");
+        login.clickLogOutButton();
     }
 }
