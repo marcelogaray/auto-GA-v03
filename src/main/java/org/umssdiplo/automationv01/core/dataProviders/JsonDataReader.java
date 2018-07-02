94ac8f9ac74f3ad586a06a452bc036163cfa3a8d
ORLANDO FERNANDEZ R <ofernandez@gmail.com>
2018-07-01 23:23:12 -0400

    ORG-TC2.2 Verificar que el codigo de organization sea unico

---
+++ src/main/java/org/umssdiplo/automationv01/core/dataProviders/JsonDataReader.java
@@ -0,0 +1,32 @@
+package org.umssdiplo.automationv01.core.dataProviders;
+
+import com.google.gson.Gson;
+import org.umssdiplo.automationv01.core.dataTypes.Organization;
+import org.umssdiplo.automationv01.core.utils.PropertyAccessor;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonDataReader {
+    private final String dataFilePath = PropertyAccessor.getInstance().getJsonDataFilePath();
+
+    public Organization getOrganizationData() {
+        String employeJsonDataFilePath = dataFilePath + "organization.json";
+        Gson gson = new Gson();
+        BufferedReader bufferReader = null;
+        try {
+            bufferReader = new BufferedReader(new FileReader(employeJsonDataFilePath));
+            Organization organization = gson.fromJson(bufferReader, Organization.class);
+            return organization;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Json file not found at path : " + employeJsonDataFilePath);
+        } finally {
+            try {
+                if (bufferReader != null) bufferReader.close();
+            } catch (IOException ignore) {
+            }
+        }
+    }
+}
