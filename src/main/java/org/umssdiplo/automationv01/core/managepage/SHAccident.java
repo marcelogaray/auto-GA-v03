cb11156a1a8cd61ace9fe17c2077f9f9c0b8dc31
Tany <tany7a@gmail.com>
2018-07-02 00:57:05 -0400

    14

--- src/main/java/org/umssdiplo/automationv01/core/managepage/SHAccident.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/SHAccident.java
@@ -8,25 +8,5 @@ import java.util.List;
 
 public class SHAccident extends BasePage {
 
-    @FindBy(id = "RegistrarAccidente")
-    private WebElement registrarAccBtn;
 
-    @FindBy(xpath = "//*[@id=\"listAccidents\"]/div/a[2]")
-    private WebElement editButton;
-
-    public SHAccident() {
-        CommonEvents.isClickable(registrarAccBtn);
-    }
-
-    public SHAccidentFormModal ClickRegistarAccBtn() {
-        CommonEvents.waitUntil(2000);
-        CommonEvents.clickButton(registrarAccBtn);
-        return new SHAccidentFormModal();
-    }
-
-    public SHAccidentEditFormModal clickEditButton() {
-        CommonEvents.waitUntil(2000);
-        CommonEvents.clickButton(editButton);
-        return new SHAccidentEditFormModal();
-    }
 }
--- src/main/java/org/umssdiplo/automationv01/core/managepage/SHAssignation.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/SHAssignation.java
@@ -6,26 +6,4 @@ import org.umssdiplo.automationv01.core.utils.CommonEvents;
 
 public class SHAssignation extends BasePage {
 
-    @FindBy(name = "new_assignment")
-    private WebElement addAssignmentButton;
-
-    @FindBy(name = "edit_assignment")
-    private WebElement editModal;
-
-    @FindBy(name = "view_assignment")
-    private WebElement viewButton;
-
-    public void isViewButtonVisible() {
-        CommonEvents.waitWebElementIsVisible(viewButton);
-    }
-
-    public SHAssignmentNewModal newAssignment() {
-        CommonEvents.clickButton(addAssignmentButton);
-        return new SHAssignmentNewModal();
-    }
-
-    public SHAssignmentEditModal clicViewEditModal() {
-        CommonEvents.clickButton(editModal);
-        return new SHAssignmentEditModal();
-    }
 }
